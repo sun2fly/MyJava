2871be9b406a48876555696bdbf67e24d950c88b
songfei20 <lysongfei@gmail.com>
2020-10-16 10:38:46 +0800

    测试用例

--- framework/concurrent/src/test/java/com/mrfsong/framework/thread/RunableTest.java
+++ framework/concurrent/src/test/java/com/mrfsong/framework/thread/RunableTest.java
@@ -4,10 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.CyclicBarrier;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 
 //AtomicStampedReference CyclicBarrier Semaphore Exchanger ReentrantReadWriteLock
@@ -100,6 +101,29 @@ public class RunableTest {
 
     }
 
+    @Test
+    public void testSemaphore() throws Exception {
+        Semaphore semaphore = new Semaphore(3,true);
+        List<Thread> runThreads = new ArrayList<>();
+        for(int i=0;i<3;i++){
+            runThreads.add(new Thread(new SemaphoreRunner(2,semaphore),"SemaphoreRunner-"+i));
+        }
+
+        for(Thread t : runThreads){
+            t.start();
+        }
+
+        //尝试等待子线程执行完成
+        TimeUnit.SECONDS.sleep(30L);
+
+
+
+
+
+
+    }
+
+
     //CyclicBarrier测试
     class CyclicBarrierRunner implements Runnable {
 
@@ -115,11 +139,10 @@ public class RunableTest {
         public void run() {
 
             try {
-
-                int seed = (int)(random.nextDouble() * 10);
+                SecureRandom random = new SecureRandom();
+                int seed = random.nextInt(10);
                 System.out.println("seed : " + seed);
                 barrier.await(seed * 100L , TimeUnit.MILLISECONDS);
-//                log.warn("Tester {} is ready !" , name);
                 System.out.println("[" + name + "] has fininshed !");
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -152,6 +175,35 @@ public class RunableTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    class SemaphoreRunner implements Runnable {
+
+        int demand;
+        Semaphore semaphore ;
+
+        public SemaphoreRunner(int demand, Semaphore semaphore) {
+            this.demand = demand;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try{
+                log.info("[" + Thread.currentThread().getName() + "] try to get " + demand + " permits , current available permits : " + semaphore.availablePermits());
+                semaphore.acquire(demand);
+                log.info("[" + Thread.currentThread().getName() + "] has got " + demand + "  permist successfully ! ");
+                int rdn = ThreadLocalRandom.current().nextInt(10) + 1;
+                TimeUnit.SECONDS.sleep(1L * rdn);
+            } catch (InterruptedException e) {
+                log.warn("[" + Thread.currentThread().getName() + "] was interrupted! ");
+            }catch (Exception e ) {
+                e.printStackTrace();
+            } finally {
+                semaphore.release(demand);
+            }
+            log.warn("[" + Thread.currentThread().getName() + "] has released " + demand + "  permists ! \n");
         }
     }
 
